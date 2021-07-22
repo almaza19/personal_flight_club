f07b55867548add799dca8ea69ef408e209d0079
Maria <al31maz@yandex.ru>
2021-07-22 23:27:26 +0300

    commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 import os
-import service
 import urllib3
 import pandas
+from googleapiclient import discovery
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -20,6 +20,7 @@ headers = {
     "apikey": API_KEY
 }
 URL = "https://tequila-api.kiwi.com/locations/query?term=paris&locale=en-US&location_types=city&active_only=true"
+SHEET_URL = f"https://sheets.googleapis.com/v4/spreadsheets/{SAMPLE_SPREADSHEET_ID}/values/{SAMPLE_RANGE_NAME}"
 
 req = requests.get(url=URL, headers=headers, verify=False)
 response_json = req.json()['locations']
@@ -31,10 +32,13 @@ for i in response_json:
         "code": IATA,
         "city": city,
     }
-sheet = service.spreadsheets()
-result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME).execute()
-values = result.get('values', [])
+credentials = None
+
+service = discovery.build('sheets', 'v4', credentials=credentials)
+request = service.spreadsheets().values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
+response = request.execute()
+print(response)
+#values = result.get('values', [])
 df = pandas.DataFrame(cities)
 
 print(df)
