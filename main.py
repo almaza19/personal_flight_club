973af99e1b4fab3c5fe4cfb433906a8144f2b6ce
Maria <al31maz@yandex.ru>
2021-07-25 19:47:33 +0300

    commit

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import os
 import urllib3
 import pandas
 from googleapiclient import discovery
+import httplib2
+import apiclient
+from oauth2client.service_account import ServiceAccountCredentials
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-
+CREDENTIALS_FILE = 'arcane-icon-318919-c0d3bef62ab4.json'
+urllib3.disable_warnings()
 API_KEY = os.environ["api_key"]
 urllib3.disable_warnings()
 IATA = []
@@ -21,24 +25,50 @@ headers = {
 }
 URL = "https://tequila-api.kiwi.com/locations/query?term=paris&locale=en-US&location_types=city&active_only=true"
 SHEET_URL = f"https://sheets.googleapis.com/v4/spreadsheets/{SAMPLE_SPREADSHEET_ID}/values/{SAMPLE_RANGE_NAME}"
+credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE,
+                                                               ['https://www.googleapis.com/auth/spreadsheets',
+                                                                'https://www.googleapis.com/auth/drive'])
+httpAuth = credentials.authorize(httplib2.Http()) # Авторизуемся в google sheets api
+# req = requests.get(url=URL, headers=headers, verify=False) #делаем запрос к tequila
+# response_json = req.json()['locations']
+# print(response_json)
+# for i in response_json:
+#     IATA.append(i["code"])
+#     city.append(i["name"])
+#
+#     cities = {
+#         "code": IATA,
+#         "city": city,
+#     }
 
-req = requests.get(url=URL, headers=headers, verify=False)
-response_json = req.json()['locations']
-for i in response_json:
-    IATA.append(i["code"])
-    city.append(i["name"])
+service = discovery.build('sheets', 'v4', http=httpAuth)
+request = service.spreadsheets().values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
+response = request.execute()['values']
+print(response)
+df_sheets = pandas.DataFrame(response, columns=['City', 'IATA Code', 'Lowest Price'])
+#values = result.get('values', [])
+df_sheets_list = df_sheets.to_dict('records')
+print("df_sheets_list", df_sheets_list) #df without index
+for c in df_sheets_list:
+    URL_city = f"https://tequila-api.kiwi.com/locations/query?term={c['City'].lower()}&locale=en-US&location_types=city&active_only=true"
+    req_1 = requests.get(url=URL_city, headers=headers, verify=False)  # делаем запрос к tequila
+    code_IATA = req_1.json()['locations'][0]['code']
+    IATA.append(code_IATA)
+    city.append(c["City"])
 
     cities = {
         "code": IATA,
         "city": city,
     }
-credentials = None
 
-service = discovery.build('sheets', 'v4', credentials=credentials)
-request = service.spreadsheets().values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
-response = request.execute()
-print(response)
-#values = result.get('values', [])
 df = pandas.DataFrame(cities)
-
+IATA_codes = [
+    cities["code"]
+]
+print(IATA_codes)
+Body = {
+    "values": [cities["code"]],
+}
+result = service.spreadsheets().values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID, body=Body, valueInputOption='RAW', range='prices!B2:B10').execute()
+print(Body)
 print(df)
